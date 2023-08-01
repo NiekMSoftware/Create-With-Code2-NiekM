25341d1870ab50199c0a57af3a8261211ad5d497
Niek Melet <niek+github@meletstudios.com>
2023-08-01 14:00:52 +0200

    Challenge 3 Completed

---
+++ Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Get the Rigidbody of the player
+        this.playerRb = this.GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKeyDown(KeyCode.Space) && !this.gameOver) {
+            this.playerRb.AddForce(Vector3.up * this.floatForce, ForceMode.Impulse);
+            print("Pressed Key");
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb")) {
+            this.gameOver = true;
+            
+            // play the SFX and Particle system
+            this.explosionParticle.Play();
+            this.playerAudio.PlayOneShot(this.explodeSound, 1.0f);
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        }
+    }
+
+}
---
+++ Challenge 3/Assets/Challenge 3/_Source_Files/SpinObjectsX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinObjectsX : MonoBehaviour
+{
+    public Vector3 rotationSpeed = new Vector3(90, 0, 0);
+
+    // Update is called once per frame
+    void Update() {
+        // transform.Rotate(Vector3.up * (this.spinSpeed * Time.deltaTime));
+    }
+}
